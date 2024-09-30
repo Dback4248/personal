fd68b2e6578791955fe089b5b7044565c3db4f5c
MUKILTEO\1721807 <atkishun000@edmonds15.org>
2024-09-30 13:38:25 -0700

    Yes

---
+++ Retro Remake/Assets/Scrips/PlayMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayMovement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+
+    private Vector2 velocity;
+    private float imputAxis;
+
+    public float moveSpeed = 8f;
+    private float inputAxis;
+
+    private void    Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        HorizontalMovement(); 
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x  =  Mathf.MoveTowards( velocity.x, inputAxis *  moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position  = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position);
+    }
+
+
+
+
+
+}
---
+++ Retro Remake/Assets/Scrips/SideScrolling.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+}
