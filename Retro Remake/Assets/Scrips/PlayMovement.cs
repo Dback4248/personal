ac6f2d4f1701990925c4ef1c321e8250e98ab0ec
MUKILTEO\1721807 <atkishun000@edmonds15.org>
2024-10-01 13:32:49 -0700

    yes

--- Retro Remake/Assets/Scrips/PlayMovement.cs
+++ Retro Remake/Assets/Scrips/PlayMovement.cs
@@ -2,17 +2,30 @@ using UnityEngine;
 
 public class PlayMovement : MonoBehaviour
 {
+    private new Camera camera;
     private new Rigidbody2D rigidbody;
 
     private Vector2 velocity;
-    private float imputAxis;
+
+    public PlayMovement(float moveSpeed)
+    {
+        this.moveSpeed = moveSpeed;
+    }
 
     public float moveSpeed = 8f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+   
+    public float JumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float Gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2f);
+
+    public bool Grounded { get; private set; }
     private float inputAxis;
 
-    private void    Awake()
+    private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
     }
 
     private void Update()
@@ -23,7 +36,7 @@ public class PlayMovement : MonoBehaviour
     private void HorizontalMovement()
     {
         inputAxis = Input.GetAxis("Horizontal");
-        velocity.x  =  Mathf.MoveTowards( velocity.x, inputAxis *  moveSpeed, moveSpeed * Time.deltaTime);
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
     }
 
     private void FixedUpdate()
@@ -31,11 +44,10 @@ public class PlayMovement : MonoBehaviour
         Vector2 position  = rigidbody.position;
         position += velocity * Time.fixedDeltaTime;
 
+        Vector2 leftEdge  = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
         rigidbody.MovePosition(position);
     }
-
-
-
-
-
 }
--- Retro Remake/Assets/Scrips/SideScrolling.cs
+++ Retro Remake/Assets/Scrips/SideScrolling.cs
@@ -8,4 +8,11 @@ public class SideScrolling : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player").transform;
     }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition  = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
 }
